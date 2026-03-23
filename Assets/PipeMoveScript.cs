3d236e60a49773bee5cba52121aa9592f0d7d3d9
Banakai1594 <167823529+AliM159@users.noreply.github.com>
2026-03-23 01:04:45 -0400

    Pipe Spawning
    Spawning pipes and delete them after it crosses the screen.

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class PipeMoveScript : MonoBehaviour
 {
 
-    public float moveSpeed = 5;
+    public float moveSpeed = 8;
+    public float deadZone = -27;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,5 +15,11 @@ public class PipeMoveScript : MonoBehaviour
     void Update()
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+
+        if (transform.position.x < deadZone)
+        {
+            Debug.Log("Pipe Deleted");
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/PipeSpawnerScript.cs
+++ Assets/PipeSpawnerScript.cs
@@ -4,15 +4,37 @@ public class PipeSpawnerScript : MonoBehaviour
 {
 
     public GameObject pipe;
+    public float spawnRate = 3;
+    private float timer = 0;
+    public float hightOffset = 10;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spawnPipe();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(pipe, transform.position, transform.rotation);
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            spawnPipe();
+            timer = 0;  
+        }
+            
+    }
+
+
+    void spawnPipe()
+    {
+        float lowestPoint = transform.position.y - hightOffset;
+        float highestPoint = transform.position.y + hightOffset;
+
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
     }
 }
