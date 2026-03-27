ea007a2c4b6020650aab13bc45ad2ccf64e02432
Banakai1594 <167823529+AliM159@users.noreply.github.com>
2026-03-27 18:02:20 -0400

    Real Game Over
    Made the bird not alive on it collides with the pipes.

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -6,6 +6,7 @@ public class BirdScript : MonoBehaviour
     public Rigidbody2D myRigidBody;
     public float flapStrength;
     public LogicScript logic;
+    public bool birdIsAlive;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,7 +16,7 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidBody.linearVelocity = Vector2.up * flapStrength;
         }
@@ -25,5 +26,6 @@ public class BirdScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         logic.gameOver();
+        birdIsAlive = false;
     }
 }
